048ca2cf272a0dfeff1b043da01050b28d02914b
Vladimir Mezentsov <vmezentsov@InComm.com>
2018-09-04 17:43:13 -0400

    Remove dummy commit data

--- src/app.js
+++ src/app.js
@@ -20,19 +20,6 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water Bill", amount: 222, createdAt: 1120 }));
-store.dispatch(addExpense({ description: "Gas Bill", amount: 80, createdAt: 1022 }));
-store.dispatch(addExpense({ description: "Cable Bill", amount: 120, createdAt: 933 }));
-store.dispatch(addExpense({ description: "Rent", amount: 1120, createdAt: 833 }));
-store.dispatch(addExpense({ description: "Coffee", amount: 20, createdAt: 144 }));
-store.dispatch(addExpense({ description: "Oil Change", amount: 40, createdAt: 47 }));
-
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-//  console.log(visibleExpenses);
-});
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
