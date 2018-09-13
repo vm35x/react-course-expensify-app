9905dc6914562d89c6e5bf9e70d2bfffad3c6848
Vladimir Mezentsov <vmezentsov@InComm.com>
2018-09-13 08:59:43 -0400

    Add expense summary to dashboard

--- src/components/ExpensifyDashboardPage.js
+++ src/components/ExpensifyDashboardPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ExpenseList from "./ExpenseList";
 import ExpenseListFilters from "./ExpenseListFilters";
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpensifyDashboardPage = () => (
   <div>
+  <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />
   </div>
---
+++ src/tests/components/ExpensesSummary.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { ExpensesSummary } from "../../components/ExpensesSummary";
+
+
+test('should correctly render espenses summary with 1 expense', () => {
+const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235} />);
+expect (wrapper).toMatchSnapshot();
+})
+
+test('should correctly render espenses summary with multiple expenses', () => {
+  const wrapper = shallow(<ExpensesSummary expenseCount={5} expensesTotal={235343} />);
+  expect (wrapper).toMatchSnapshot();
+})
